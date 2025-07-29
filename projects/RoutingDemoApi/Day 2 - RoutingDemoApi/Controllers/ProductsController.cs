9b66a0bfcb8dcb30194bf19ad5d3d0cbb6eac7d8
UminduCorzent <umindu@corzent.com>
2025-07-29 16:34:41 +0530

    Simple project to understand basics of routing and controllers

---
+++ projects/RoutingDemoApi/Day 2 - RoutingDemoApi/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day_2___RoutingDemoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = new[]
+            {
+                new { Id = 1, Name = "Laptop" },
+                new { Id = 2, Name = "Phone" }
+            };
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = new { Id = id, Name = $"Product {id}" };
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(int id, string name)
+        {
+            var product = new { Id = id, Name = name };
+            return CreatedAtAction(nameof(GetById),new { id }, product);
+        }
+    }
+}
